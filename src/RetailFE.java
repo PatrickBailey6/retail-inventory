f32595dd0a00bd6f47dfc2b5b7a7061ce773785d
PatrickBailey6 <pab6@email.sc.edu>
2024-09-24 14:44:14 -0400

    Front End progress

---
+++ src/RetailFE.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class RetailFE {
+    public static void main (String[] args) throws Exception {
+
+        String cFileDirectory = System.getProperty ("user.dir");
+
+        while (true)
+        {
+            System.out.println("Starting Retail Inventory.");
+            try
+            {
+                System.out.println("\n Searching for existing presets.");
+                Scanner fileReader = new Scanner (new File (cFileDirectory+"/user_presets"));
+                // TODO design preset file 
+                fileReader.close();
+
+            }
+            catch (Exception e)
+            {
+                System.out.println("File not found; Creating new preset file");
+                // TODO implement preset file here
+                PrintWriter fileWriter = new PrintWriter(new FileOutputStream(cFileDirectory));
+                fileWriter.close();
+            }
+        
+            
+        }
+    }
+}
+
+
